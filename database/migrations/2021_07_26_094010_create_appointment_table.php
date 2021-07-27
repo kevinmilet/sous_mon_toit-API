2594a61e95d9dbeeda3705447df56bfcb0385192
Kevin Milet <kevin.milet@gmail.com>
2021-07-27 09:40:52 +0200

    Kevin - correction create_appointment_table.php migration file

--- database/migrations/2021_07_26_094010_create_appointment_table.php
+++ database/migrations/2021_07_26_094010_create_appointment_table.php
@@ -18,21 +18,15 @@ class CreateAppointmentTable extends Migration
             $table->string('appointment_type');
         });
 
-        Schema::create('appointments', function (Blueprint $table) { 
+        Schema::create('appointments', function (Blueprint $table) {
             $table->id('id');
             $table->dateTime('scheduled_at');
             $table->text('notes')->nullable();
-<<<<<<< HEAD
-            $table->foreignId('id_estate')->constrained()->references('id')->on('estates');;
-            $table->foreignId('id_staffs')->constrained()->references('id')->on('staffs');;
-            $table->foreignId('id_customer')->constrained()->references('id')->on('customers');;
-            $table->foreignId('id_appointment_type')->constrained('appointment_types')->references('id')->on('appointment_types');;
-=======
             $table->foreignId('id_estate')->constrained()->references('id')->on('estates');
             $table->foreignId('id_staffs')->constrained()->references('id')->on('staffs');
             $table->foreignId('id_customer')->constrained()->references('id')->on('customers');
             $table->foreignId('id_appointment_type')->constrained('appointment_types')->references('id')->on('appointment_types');
->>>>>>> origin/stephane
+
         });
     }
 
