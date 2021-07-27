ec4288615da02f1ac02a05d1a75b23d7ff8b4ad0
ethan-eldib <ethan.eldib@gmail.com>
2021-07-27 09:33:21 +0200

    Ethan - modification fichier DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(InsterEstatesTypesSeeder::class);
+        $this->call(InsertEstatesTypesSeeder::class);
         $this->call(InsertEstatesSeeder::class);
     }
 }
