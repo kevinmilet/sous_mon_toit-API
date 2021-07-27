ed5523cd790f01c45d9d5de848887d6db32779ab
Kevin Milet <kevin.milet@gmail.com>
2021-07-27 10:14:51 +0200

    Kevin - ajout seeder customer_search / correction nom classe InsertAppointmentsSeeder.php, InsertAppointmentTypesSeeder.php / correction InsertCustomerSeeder.php

---
+++ database/seeders/InsertCustomerSearchSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class InsertCustomerSearchSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('customer_search')->insert([
+            [
+                'id' => 1,
+                'buy_or_rent' => 'Achat',
+                'surface_min' => '100',
+                'number_rooms' => '5',
+                'budget_min' => '100000',
+                'budget_max' => '150000',
+                'search_longitude' => '2.3',
+                'search_latitude'=> '49.9',
+                'search_radius' => '20',
+                'created_at' => '2021-07-26 10:00:00',
+                'updated_at' => null,
+                'alert' => true,
+                'id_customer' => 1,
+            ],
+            [
+                'id' => 2,
+                'buy_or_rent' => 'Location',
+                'surface_min' => '70',
+                'number_rooms' => '3',
+                'budget_min' => '500',
+                'budget_max' => '800',
+                'search_longitude' => '2.3',
+                'search_latitude'=> '49.9',
+                'search_radius' => '20',
+                'created_at' => '2021-07-26 10:00:00',
+                'updated_at' => null,
+                'alert' => true,
+                'id_customer' => 2,
+            ],
+        ]);
+    }
+}
