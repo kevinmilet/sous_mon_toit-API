499d456d94c2c1eda0383d2e833a372d8f7f459a
ethan-eldib <ethan.eldib@gmail.com>
2021-07-27 15:17:36 +0200

    Ethan - fix web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@
 $router->group(['prefix' => 'biens'], function () use ($router) {
     $router->get('/', 'EstatesController@selectAllEstates');
     $router->get('/{id}', 'EstatesController@selectOneEstate');
+    $router->patch('/archive/{id}', 'EstatesController@archive');
 });
