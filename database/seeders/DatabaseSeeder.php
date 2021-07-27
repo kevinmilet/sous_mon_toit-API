cd3b4c0ef5ec286aff9140e2d67f774d0e0d7bae
ethan-eldib <ethan.eldib@gmail.com>
2021-07-27 08:57:00 +0200

    Ethan - ajout des fichiers InsertEstatesSeeder.php + InsertEstatesTypesSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,8 +11,9 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // $this->call('UsersTableSeeder');
+        $this->call(InsterEstatesTypesSeeder::class);
+        $this->call(InsertEstatesSeeder::class);
     }
 }
