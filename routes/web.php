8ca2becb97255f3f2576db1407af93954f34f786
ethan-eldib <ethan.eldib@gmail.com>
2021-07-27 15:09:21 +0200

    Ethan - correction fichier web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 /** @var \Laravel\Lumen\Routing\Router $router */
-
-$router->get('/biens', 'EstatesController@selectAllEstates');
-
 $router->group(['prefix' => 'biens'], function () use ($router) {
     $router->get('/', 'EstatesController@selectAllEstates');
     $router->get('/{id}', 'EstatesController@selectOneEstate');
