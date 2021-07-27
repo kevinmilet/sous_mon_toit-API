bd4ff22d013ead7b58dcc71e651468f3aae3bb11
StephaneC <stephane.cavillon@itech.fr>
2021-07-27 12:44:41 +0200

    fixed appointment route

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ $router->get('/', function () use ($router) {
 
 //Appointment
 $router->group(['prefix' => 'schedule'], function () use ($router){
-    $router->get('/', showAllAppointment()); // /schedule/
-    $router->get('{appointment_id}', showAppointment($appointment_id)); // /schedule/{appointment_id}
-    $router->get('customer/{customer_id}', showCustomerAppointment($customer_id)); // /scheduled/customer/{customer_id}
-    $router->get('staff/{staff_id}', showStaffAppointment($staff_id)); // /scheduled/customer/{staff_id}
-    $router->post('createAppt', createAppointment()); // /schedule/createAppt
-    $router->patch('update', updateAppointment()); // /schedule/update
-    $route->delete('delete/{appointment_id}', deleteAppointment($appointment_id)); // /schedule/delete/{appointment_id}
+    $router->get('/', 'appointmentCtrl@showAllAppointments'); // /schedule/
+    $router->get('{appointment_id}', 'appointmentCtrl@showAppointment'); // /schedule/{appointment_id}
+    $router->get('customer/{customer_id}', 'appointmentCtrl@showCustomerAppointment'); // /scheduled/customer/{customer_id}
+    $router->get('staff/{staff_id}', 'appointmentCtrl@showStaffAppointment'); // /scheduled/customer/{staff_id}
+    $router->post('createAppt', 'appointmentCtrl@createAppointment'); // /schedule/createAppt
+    $router->patch('update', 'appointmentCtrl@updateAppointment'); // /schedule/update
+    $route->delete('delete/{appointment_id}', 'appointmentCtrl@deleteAppointment'); // /schedule/delete/{appointment_id}
 })
