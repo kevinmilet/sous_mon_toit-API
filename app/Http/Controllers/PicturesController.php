cfa560012b3650eb469a302f600b31a7a43019d8
Kevin Milet <kevin.milet@gmail.com>
2021-07-27 15:51:30 +0200

    Kevin - ...

--- app/Http/Controllers/PicturesController.php
+++ app/Http/Controllers/PicturesController.php
@@ -4,7 +4,14 @@
 namespace App\Http\Controllers;
 
 
-class Pictures
-{
+use App\Models\Pictures;
+use App\Models\Staffs;
+use Illuminate\Http\JsonResponse;
 
+class PicturesController extends Controller
+{
+    public function getOnePicture($id): JsonResponse
+    {
+        return response()->json(Pictures::find($id));
+    }
 }
--- app/Models/Pictures.php
+++ app/Models/Pictures.php
@@ -1,1 +1,28 @@
 <?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+
+/**
+ * @method static find($id)
+ */
+class Pictures extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable, HasFactory;
+
+    protected $fillable = [
+        'id', 'folder', 'name', 'cover', 'alt', 'id_estate',
+    ];
+
+    protected $hidden = [
+    ];
+
+
+
+}
