05ec0a9f23b114ff3153a3b9a00bca9e5d3f5fab
StephaneC <stephane.cavillon@itech.fr>
2021-07-27 12:37:53 +0200

    adding appointment route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+//Appointment
+$router->group(['prefix' => 'schedule'], function () use ($router){
+    $router->get('/', showAllAppointment()); // /schedule/
+    $router->get('{appointment_id}', showAppointment($appointment_id)); // /schedule/{appointment_id}
+    $router->get('customer/{customer_id}', showCustomerAppointment($customer_id)); // /scheduled/customer/{customer_id}
+    $router->get('staff/{staff_id}', showStaffAppointment($staff_id)); // /scheduled/customer/{staff_id}
+    $router->post('createAppt', createAppointment()); // /schedule/createAppt
+    $router->patch('update', updateAppointment()); // /schedule/update
+    $route->delete('delete/{appointment_id}', deleteAppointment($appointment_id)); // /schedule/delete/{appointment_id}
+})
