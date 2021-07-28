0f28befda470c72c3f3c9abb2cfbe35998da77ed
Kevin Milet <kevin.milet@gmail.com>
2021-07-28 15:47:54 +0200

    Kevin - ...

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,28 @@
 
 use Laravel\Lumen\Routing\Router;
 
-$router->group(['prefix' => 'biens'], function () use ($router) {
+// Biens
+$router->group(['prefix' => 'estates'], function () use ($router) {
     $router->get('/', 'EstatesController@selectAllEstates'); // /biens/
     $router->get('/{id}', 'EstatesController@selectOneEstate'); // /biens/{id}
     $router->patch('/archive/{id}', 'EstatesController@archive'); // /biens/archive/{id}
 });
 
-//Appointment
-$router->group(['prefix' => 'schedule'], function () use ($router){
-    $router->get('/', 'AppointmentsController@showAllAppointments'); // /schedule/
-    $router->get('{appointment_id}', 'AppointmentsController@showAppointment'); // /schedule/{appointment_id}
-    $router->get('customer/{customer_id}', 'AppointmentsController@showCustomerAppointment'); // /scheduled/customer/{customer_id}
-    $router->get('staff/{staff_id}', 'AppointmentsController@showStaffAppointment'); // /scheduled/customer/{staff_id}
-    $router->post('createAppt', 'AppointmentsController@createAppointment'); // /schedule/createAppt
-    $router->put('update/{appointment_id}', 'AppointmentsController@updateAppointment'); // /schedule/update/{appointment_id} (data à passer en params)
-    $router->delete('delete/{appointment_id}', 'AppointmentsController@deleteAppointment'); // /schedule/delete/{appointment_id}
+// Types de biens
+$router->group(['prefix' => 'estates_types'], function () use ($router) {
+    $router->get('/', 'EstatesTypesController@getAllEstatesTypes'); // /estates_types/
 });
 
-//AppointmentType
-$router->get('apptTypes', 'AppointmentsTypesController@showAllTypes'); // /apptTypes
+//Appointment
+$router->group(['prefix' => 'schedule'], function () use ($router) {
+    $router->get('/', 'appointmentsController@showAllAppointments'); // /schedule/
+    $router->get('{appointment_id}', 'appointmentsController@showAppointment'); // /schedule/{appointment_id}
+    $router->get('customer/{customer_id}', 'appointmentsController@showCustomerAppointment'); // /scheduled/customer/{customer_id}
+    $router->get('staff/{staff_id}', 'appointmentsController@showStaffAppointment'); // /scheduled/customer/{staff_id}
+    $router->post('createAppt', 'appointmentsController@createAppointment'); // /schedule/createAppt
+    $router->put('update/{appointment_id}', 'appointmentsController@updateAppointment'); // /schedule/update/{appointment_id} (data à passer en params)
+    $router->delete('delete/{appointment_id}', 'appointmentsController@deleteAppointment'); // /schedule/delete/{appointment_id}
+});
 
 /*
  *  Routes pour Staffs
@@ -30,7 +33,9 @@ $router->get('apptTypes', 'AppointmentsTypesController@showAllTypes'); // /apptT
 $router->group(['prefix' => 'staff'], function () use ($router) {
     $router->get('/', 'StaffsController@getAllStaff'); // /staff/
     $router->get('/{id}', 'StaffsController@getOneById'); // /staff/{id}
-    $router->patch('/archive/{id}', 'StaffsController@archive'); // /staff/archive/{id}
+    $router->delete('/delete/{id}', 'StaffsController@delete'); // /staff/delete/{id}
+    $router->post('/create/', 'StaffsController@create'); // /staff/create
+    $router->put('/update/{id}', 'StaffsController@update'); // /staff/update/{id}
 });
 
 /*
@@ -49,15 +54,20 @@ $router->group(['prefix' => 'roles'], function () use ($router) {
 
 // Customers
 $router->group(['prefix' => 'customer'], function () use ($router) {
-    $router->get('/','CustomersController@selectAllCustomers');
+    $router->get('/', 'CustomersController@selectAllCustomers');
     $router->get('/{id}', 'CustomersController@selectOneCustomer');
-    $router->patch('/archive/{id}','CustomersController@archive');
+    $router->post('create','CustomersController@create');
+    $router->put('update/{id}','CustomersController@update');
+    $router->delete('delete/{id}', 'CustomersController@delete');
 });
 
 //Contract
-$router->group(['prefix' => 'contract'], function () use ($router){
+$router->group(['prefix' => 'contract'], function () use ($router) {
     $router->get('/', 'ContractsController@selectAllContracts');
     $router->get('/contractsTypes', 'ContractsTypesController@selectAllContractsTypes');
-    $router->get('/{id_contract}', 'ContractsController@selectOneContract');
+    $router->get('/createForm', 'ContractsController@createContractView');
+    $router->post('/saveContract', 'ContractsController@saveNewContract');
+    $router->put('/updateContract', 'ContractsController@updateContract');
     $router->patch('/archive/{id_contract}', 'ContractsController@archiveContract');
+    $router->get('/{id_contract}', 'ContractsController@selectOneContract');
 });
