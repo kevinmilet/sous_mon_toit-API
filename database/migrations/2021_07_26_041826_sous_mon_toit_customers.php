16f329c12de021acefcb87b502e659695ef01439
Kevin Milet <kevin.milet@gmail.com>
2021-07-28 15:41:12 +0200

    Kevin - ...

--- database/migrations/2021_07_26_041826_sous_mon_toit_customers.php
+++ database/migrations/2021_07_26_041826_sous_mon_toit_customers.php
@@ -30,16 +30,8 @@ class SousMonToitCustomers extends Migration
             $table->integer('first_met');
             $table->string('token', 255)->nullable();
             $table->integer('password_request')->nullable();
-<<<<<<< HEAD
         });
             }
-=======
-
-        });
-   
-        //
-    }
->>>>>>> origin/laurent
 
     /**
      * Reverse the migrations.
