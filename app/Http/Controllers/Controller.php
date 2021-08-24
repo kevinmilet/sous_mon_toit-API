4b7cce17606f926723288993ad47be308f71aba3
AnneCrepin <anne.crepin62@gmail.com>
2021-08-24 09:14:21 +0200

    installation de swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
+use OpenApi\Annotations as OA;
+
 
 class Controller extends BaseController
 {
+
+    /** 
+    * @OA\Info(title="Ma première API", version="0.1") 
+    * @OA\Server(
+    *      url="http://localhost:8000",
+    *      description = "API de l'agence immobiliere Sous Mon Toit",
+    * )
+    */
     protected function respondWithToken($token): \Illuminate\Http\JsonResponse
     {
         return response()->json([
